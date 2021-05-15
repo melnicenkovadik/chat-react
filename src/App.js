a8d99d0eeaea66e9b4b49dcc936a1ea8f701d5ad
melnicenkovadik <melnicenkovadik@gmail.com>
2021-05-15 10:16:55 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import firebase from 'firebase/app'
+import  'firebase/firestore'
+import  'firebase/auth'
+import {} 'react-firebase-hooks/auth'
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                sad
+            </header>
+        </div>
+    );
+}
+
+export default App;
